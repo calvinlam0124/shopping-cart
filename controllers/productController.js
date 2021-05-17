089f5e680479549a75a5fc93160519d09eef0781
Karol <super000999888@gmail.com>
2021-05-17 20:27:24 +0800

    add cart list in getProducts page

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,6 @@
 const db = require('../models')
 const Product = db.Product
+const Cart = db.Cart
 
 const PAGE_OFFSET = 0
 const PAGE_LIMIT = 3
@@ -13,6 +14,15 @@ const productController = {
         offset: PAGE_OFFSET,
         limit: PAGE_LIMIT
       })
+
+      if (req.session.cartId) {
+        let cart = await Cart.findByPk(req.session.cartId, {
+          include: 'cartProducts'
+        })
+        cart = cart.toJSON()
+        const totalPrice = cart.cartProducts.length > 0 ? cart.cartProducts.map(d => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
+        return res.render('products', { products, cart, totalPrice })
+      }
       return res.render('products', { products })
     } catch (e) {
       console.log(e)
