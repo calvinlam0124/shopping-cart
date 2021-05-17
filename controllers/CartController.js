7c320cdbd36f59ebfdefc9266f33ad4d34ec3f27
Karol <super000999888@gmail.com>
2021-05-17 17:39:24 +0800

    add addCartItem route

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -5,11 +5,14 @@ const CartItem = db.CartItem
 const cartController = {
   getCart: async (req, res) => {
     try {
-      const cart = await Cart.findByPk(req.session.cartId, {
-        include: 'cartProducts'
-      })
-      const totalPrice = cart.cartProducts.length > 0 ? cart.cartProducts.map(d => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
-      return res.render('cart', { cart: cart.toJSON(), totalPrice })
+      if (req.session.cartId) {
+        const cart = await Cart.findByPk(req.session.cartId, {
+          include: 'cartProducts'
+        })
+        const totalPrice = cart.cartProducts.length > 0 ? cart.cartProducts.map(d => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
+        return res.render('cart', { cart: cart.toJSON(), totalPrice })
+      }
+      return res.render('cart')
     } catch (e) {
       console.log(e)
     }
@@ -34,6 +37,18 @@ const cartController = {
       await product.save()
       // save cartId in session
       req.session.cartId = cart.id
+      return res.redirect('back')
+    } catch (e) {
+      console.log(e)
+    }
+  },
+  addCartItem: async (req, res) => {
+    try {
+      // find cart
+      const product = await CartItem.findByPk(req.params.productId)
+      await product.update({
+        quantity: product.quantity + 1
+      })
       return res.redirect('back')
     } catch (e) {
       console.log(e)
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const products = require('./modules/products')
 const carts = require('./modules/carts')
+const cartItems = require('./modules/cartItems')
 
 module.exports = app => {
   app.use('/products', products)
   app.use('/cart', carts)
+  app.use('/cartItem', cartItems)
 }
