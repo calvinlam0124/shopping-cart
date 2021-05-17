003fdf084b95fd51f1943bb24d06c719db2f848c
Karol <super000999888@gmail.com>
2021-05-17 14:51:25 +0800

    modify getCart

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -4,9 +4,11 @@ const Cart = db.Cart
 const cartController = {
   getCart: async (req, res) => {
     try {
-      const cart = await Cart.findOne({ include: 'items' })
-      const totalPrice = cart.items.length > 0 ? cart.items.map(d => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
-      return res.render('cart', { cart, totalPrice })
+      const cart = await Cart.findOne({
+        include: 'cartProduct'
+      })
+      const totalPrice = cart.cartProducts.length > 0 ? cart.cartProducts.map(d => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
+      return res.render('cart', { cart: cart.toJSON(), totalPrice })
     } catch (e) {
       console.log(e)
     }
