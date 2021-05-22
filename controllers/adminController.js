07a37bef32228f02e6f6697eda4afd7eef7a4554
Karol <super000999888@gmail.com>
2021-05-22 16:52:13 +0800

    modify postProduct route for image upload by multer and imgur

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,20 @@
 const db = require('../models')
 const Product = db.Product
 
+// imgur
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+const uploadImg = path => {
+  return new Promise((resolve, reject) => {
+    imgur.upload(path, (err, img) => {
+      if (err) {
+        return reject(err)
+      }
+      return resolve(img)
+    })
+  })
+}
+
 const adminController = {
   // get all products
   getProducts: async (req, res) => {
@@ -17,11 +31,17 @@ const adminController = {
   // create new product
   postProduct: async (req, res) => {
     try {
-      const { name, description, price, image } = req.body
+      const { name, description, price } = req.body
       // if (!name || !price) {
       //   return res.redirect('')
       // }
-      await Product.create({ name, description, price, image })
+      if (req.file) {
+        imgur.setClientID(IMGUR_CLIENT_ID)
+        const img = await uploadImg(req.file.path)
+        await Product.create({ name, description, price, image: img.data.link })
+      } else {
+        await Product.create({ name, description, price })
+      }
       return res.redirect('/admin/products')
     } catch (e) {
       console.log(e)
--- routes/modules/admins.js
+++ routes/modules/admins.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
 
 const adminController = require('../../controllers/adminController')
 
 router.get('/products', adminController.getProducts)
-router.post('/products', adminController.postProduct)
+router.post('/products', upload.single('image'), adminController.postProduct)
 
 module.exports = router
