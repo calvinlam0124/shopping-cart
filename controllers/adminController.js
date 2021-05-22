d2175d51a62955e6c93499bb753980708d7bd1ae
Karol <super000999888@gmail.com>
2021-05-22 17:59:04 +0800

    add editProduct & putProduct routes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -19,6 +19,9 @@ const adminController = {
   // get all products
   getProducts: async (req, res) => {
     try {
+      // status (default: create)
+      // req.session.status = ''
+      // const status = 'create'
       const products = await Product.findAll({
         raw: true,
         nest: true
@@ -41,6 +44,43 @@ const adminController = {
         await Product.create({ name, description, price, image: img.data.link })
       } else {
         await Product.create({ name, description, price })
+      }
+      return res.redirect('/admin/products')
+    } catch (e) {
+      console.log(e)
+    }
+  },
+  // edit product page
+  editProduct: async (req, res) => {
+    try {
+      // set status
+      const status = 1
+      // find the product
+      const product = await Product.findByPk(req.params.id)
+      // find products
+      const products = await Product.findAll({
+        raw: true,
+        nest: true
+      })
+      return res.render('admin/products', { product: product.toJSON(), products, status })
+    } catch (e) {
+      console.log(e)
+    }
+  },
+  // edit product
+  putProduct: async (req, res) => {
+    try {
+      const { name, description, price } = req.body
+      // if (!name || !price) {
+      //   return res.redirect('')
+      // }
+      const product = await Product.findByPk(req.params.id)
+      if (req.file) {
+        imgur.setClientID(IMGUR_CLIENT_ID)
+        const img = await uploadImg(req.file.path)
+        await product.update({ name, description, price, image: img.data.link })
+      } else {
+        await product.update({ name, description, price, image: product.image })
       }
       return res.redirect('/admin/products')
     } catch (e) {
--- routes/modules/admins.js
+++ routes/modules/admins.js
@@ -7,5 +7,7 @@ const adminController = require('../../controllers/adminController')
 
 router.get('/products', adminController.getProducts)
 router.post('/products', upload.single('image'), adminController.postProduct)
+router.get('/products/:id', adminController.editProduct)
+router.put('/products/:id', upload.single('image'), adminController.putProduct)
 
 module.exports = router
