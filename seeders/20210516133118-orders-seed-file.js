d372b63afe0a5eca42bfec1ab32db0f88d0ea7e3
Karol <super000999888@gmail.com>
2021-05-23 12:24:02 +0800

    add users seed

--- seeders/20210516133118-orders-seed-file.js
+++ seeders/20210516133118-orders-seed-file.js
@@ -1,17 +1,17 @@
 'use strict'
 
 const faker = require('faker')
-// const db = require('../models')
-// const User = db.User
+const db = require('../models')
+const User = db.User
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // const users = await User.findAll()
+    const users = await User.findAll({ where: { role: 'user' } })
 
     await queryInterface.bulkInsert('Orders',
       Array.from({ length: 2 }).map((item, index) => ({
         // id: index + 1,
-        // UserId: users[Math.floor(Math.random() * users.length)],
+        UserId: users[Math.floor(Math.random() * users.length)].id,
         name: faker.name.findName(),
         phone: faker.phone.phoneNumberFormat(),
         address: faker.address.streetAddress(),
