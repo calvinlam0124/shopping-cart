c36ed0612ec47e6c491a1b55e694348f707ec3aa
Karol <super000999888@gmail.com>
2021-05-29 16:09:28 +0800

    change name of CartController

--- routes/modules/carts.js
+++ routes/modules/carts.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const { authenticated } = require('../../middleware/auth')
 
-const cartController = require('../../controllers/cartController')
+const cartController = require('../../controllers/CartController')
 
 router.get('/', authenticated, cartController.getCart)
 router.post('/', authenticated, cartController.postCart)
