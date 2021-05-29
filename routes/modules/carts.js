4f7686e5d118b6a52ffb0213b294c43a79a8b03c
Karol <super000999888@gmail.com>
2021-05-29 15:28:06 +0800

    fix carts module

--- routes/modules/carts.js
+++ routes/modules/carts.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const cartController = require('../../controllers/cartController')
-
 const { authenticated } = require('../../middleware/auth')
+
+const cartController = require('../../controllers/cartController')
 
 router.get('/', authenticated, cartController.getCart)
 router.post('/', authenticated, cartController.postCart)
